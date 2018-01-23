babf1bf0cd4a1e9dc83c3c87d56bf3126be1e30b
Ryan Schmelter <rschmelter33@gmail.com>
2018-01-23 00:06:43 +0000

    automatically backed up by learn

--- lib/a.rb
+++ lib/a.rb
@@ -8,7 +8,20 @@ def start
   show_states
 end
 
-def welcome
-  
-  
+def show_states
+  puts "Welcome! To get started, type 'list' to show a list of states and areas with available rentals"
+    valid = false
+    while valid == false
+      input = gets.strip
+      if input.downcase == "list"
+        valid = true
+        i = 1
+        @states_array.each do |state|
+          puts "#{i}. #{state.name}"
+          i += 1
+        end
+      else
+        puts "Invalid input. Please type 'list' to show states"
+      end
+    end
 end
